8d9b2988b61b5e58aca6c4b0ce59ffd0f95371d4
nlf <nlf@nlf.sh>
2023-06-22 12:02:01 -0700

    fix: use the right return value

--- test/types.ts
+++ test/types.ts
@@ -5,13 +5,11 @@ import { getGeneratorOptions } from './fixtures/util';
 import { generateTypesFile } from '../lib/types';
 
 void t.test('generates types', async (t) => {
-  const { root, options } = await getGeneratorOptions(t);
+  const options = await getGeneratorOptions(t);
 
   const project = new Project(options.projectSettings);
   generateTypesFile(project, options);
   await project.save();
-
-  console.error(project.getSourceFiles());
 
   t.end();
 });
